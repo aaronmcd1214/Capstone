7dd9bf7de9aceca5cbea0170f9355390f083cd77
Aaron McDonald <aaronmcdonald@Aarons-MacBook-Pro.local>
2018-05-01 16:28:11 -0700

    done for the day

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,4 +1,4 @@
-import pandas as import pd
+import pandas as pd
 import numpy as np
 
 def initial_drop(df):
@@ -8,6 +8,8 @@ def initial_drop(df):
 
     Args:
         df: pandas DataFrame object
+    Returns:
+        nothing, df is acted on inplace
     """
     drop_list = ['teamID', 'lgID', 'stint']
     df.drop(drop_list, inplace=True, axis=1) #cols are dropped inplace, no return necessary
@@ -26,6 +28,25 @@ def combine_stints(df):
     df = df.groupby(['playerID', 'yearID']).sum()
     return df.reset_index(level=['playerID', 'yearID'])
 
+def map_position(batting_df, fielding_df):
+    """
+    This function will use a DataFrame of fielding statistics to find each players'
+    most played position and then add that position to the batting_df
+
+    Args:
+        batting_df: pandas DataFrame object
+        fielding_df: pandas DataFrame object
+    Returns:
+        nothing, batting_df is acted on inplace
+    """
+    for player in fielding_df['playerID'].unique():
+        idxmax = fielding_df[fielding_df['playerID'] == player]['POS'].value_counts().idxmax()
+        batting_df.loc[batting_df['playerID'] == player, 'pos'] = idxmax
+    batting_df_dropped_p = batting_df.drop(batting_df[batting_df['pos'] == 'P'].index) #drop pitchers
+    batting_df_dropped_p_null = batting_df_dropped_p.dropna() #drop players with no position
+
+    return batting_df_dropped_p_null
+
 def create_averages(df):
     """
     This function creates a new DataFrame that contains each player's averages
@@ -39,4 +60,4 @@ def create_averages(df):
     avg_df = df.drop('yearID', axis=1)
     avg_df = avg_df.groupby('playerID').mean()
     avg_df['total_seasons'] = [len(df[df['playerID'] == player]) for player in df['playerID'].unique()]
-    return avg_df
+    return avg_df.round(3)
---
+++ src/fetch_data.py
@@ -0,0 +1,96 @@
+from data_cleaning as dc
+import pandas as pd
+import sys
+
+class PlayerPrediction(object):
+    def __init__(self, player_name):
+        self.player_name = player_name
+        self.playerID = get_id(player_name)
+        self.build_dataframes()
+        build_models()
+        predict()
+        display_results()
+
+    def get_id(player_name):
+        pass
+
+    def build_dataframes(self):
+        #initial DataFrames created
+        print('Building DataFrames and engineering features/cleaning...')
+        master_df = pd.read_csv('../data/baseballdatabank-master/core/People.csv')
+        batting_df = pd.read_csv('../data/baseballdatabank-master/core/Batting.csv')
+        fielding_df = pd.read_csv('../data/baseballdatabank-master/core/Fielding.csv')
+        #initial feature drop
+        dc.initial_drop(batting_df)
+        #combine stints to single years
+        print('Combining multiple stints into single years...')
+        batting_df_stints_combined = dc.combine_stints(batting_df)
+        #create avg_df, contains players' averages over career
+        print('Creating DataFrame of {}\'s averages...'.format(self.player_name))
+        self.avg_df = dc.create_averages(batting_df_stints_combined)
+        #map most-played position to the batting_df and drop pitchers and those without a position
+        print('Mapping positions to batting stats...')
+        self.batting_pos_df = dc.map_position(batting_df_stints_combined, fielding_df)
+        #pull out players of same position and at least as many years as self.playerID
+        players_to_compare = get_comparison(batting_pos_df)
+
+    def _get_comparison(self, df):
+        """
+        This function receives a DataFrame and returns a new DataFrame that contains
+        the players who have as much experience as the player to be projected
+
+        Args:
+            df: pandas DataFrame object
+        Returns:
+            filtered DataFrame
+        """
+        #get player's position
+        player_pos = df.loc[df['playerID'] == self.playerID, 'pos'].unique()[0]
+
+        #set masks
+        player_mask = df['playerID'] == self.playerID
+        pos_mask = df['pos'] == player_pos
+
+        #get years of service
+        years_served = len(df[player_mask])
+
+        #get players of same position
+        same_pos = df.loc[(~player_mask) & (pos_mask)]
+
+        #filter players of same position and their first n years equal to self.playerID's service time
+        first_n_years = same_pos.groupby('playerID', as_index=False).head(years_served)
+
+        #filter out players with service time less than self.playerID
+        return players_to_compare_func(first_n_years)
+
+    def _players_to_compare_func(self, df):
+        """
+        This function filters the passed in DataFrame to remove players who don't
+        have at least the same amount of years of service as the player to be projected
+
+        Args:
+            df: pandas DataFrame object
+        Returns:
+            filtered DataFrame
+        """
+        value_counts = first_n_years.playerID.value_counts()
+        players_to_compare = value_counts.loc[value_counts.values == 7].index
+        return first_n_years[first_n_years['playerID'].isin(players_to_compare)]
+
+
+    def build_models(self, playerID):
+        pass
+
+    def predict(self):
+        pass
+
+    def display_results(self):
+        pass
+
+if __name__ == '__main__':
+    """
+    Code will be built here so that the script can be run from the command line
+    as such: python fetch_data.py <player name>.  This script will then use the
+    provided player name to find similar players and predict future offensive
+    production
+    """
