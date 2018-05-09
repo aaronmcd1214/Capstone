7ab43a0df16e65df3e12d8e7915d46acb82f6ee5
Aaron McDonald <aaronmcdonald@Aarons-MacBook-Pro.local>
2018-05-09 12:34:48 -0700

    functionalized condense_df

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 def initial_drop(df):
     """
@@ -9,7 +10,7 @@ def initial_drop(df):
     Args:
         df: pandas DataFrame object
     Returns:
-        nothing, df is acted on inplace
+        pandas DataFrame
     """
     drop_list = ['teamID', 'lgID', 'stint']
     return df.drop(drop_list, axis=1) #cols are dropped inplace, no return necessary
@@ -38,7 +39,7 @@ def map_position(batting_df, fielding_df):
         batting_df: pandas DataFrame object
         fielding_df: pandas DataFrame object
     Returns:
-        nothing, batting_df is acted on inplace
+        pandas DataFrame
     """
     print('Mapping positions to batting stats...')
     for player in fielding_df['playerID'].unique():
@@ -48,6 +49,45 @@ def map_position(batting_df, fielding_df):
     batting_df_dropped_p_null = batting_df_dropped_p.dropna()  # drop players with no position
 
     return batting_df_dropped_p_null
+
+def condense_df(df0):
+    """
+    This function condenses all the rows of a players stats over time into one
+    row of stats over time
+
+    Args:
+        df0: pandas DataFrame
+    Returns:
+        pandas DataFrame
+    """
+    df = df0.set_index('playerID')
+    player_list = []
+    for player in df.index.unique():
+        def_dict = defaultdict(str)
+        def_dict['playerID'] = player
+
+        if isinstance(df.loc[player, 'pos'], str):
+            def_dict['pos'] = df.loc[player, 'pos']
+        else:
+            def_dict['pos'] = df.loc[player, 'pos'].unique()[0]
+
+        for col in df.columns:
+            if col == 'yearID' or col == 'pos':
+                continue
+            try:
+                for i, v in enumerate(df.loc[player, col]):
+                    if i > 6:
+                        continue
+                    key = 'year' + str(i + 1) + '_' + col
+                    def_dict[key] = v
+            except:
+                continue # skip players with only 1 year of data
+        player_list.append(def_dict)
+    new_df = pd.DataFrame(player_list)
+    new_df_na_dropped = new_df.dropna() #drop players without 7 years of data
+    new_df_w_dummies = pd.get_dummies(new_df_na_dropped, columns=['pos'])
+    return new_df_w_dummies
+
 # probably not needed
 # def create_averages(df):
 #     """
